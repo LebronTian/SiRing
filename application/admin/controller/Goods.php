0fb937551ab98824db56c44c0bf6587001c9fc4d
Your Name <you@example.com>
2018-08-02 18:33:09 +0800

    陈绪

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -9,6 +9,8 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Request;
+
 class Goods extends Controller{
     /**
      * [商品列表]
@@ -20,6 +22,34 @@ class Goods extends Controller{
     }
 
     public function add(){
+
         return view("goods_add");
+    }
+
+    public function save(Request $request){
+       if ($request->isPost()){
+           
+           $goods_data = $request->only([
+                        "goods_name",
+                        "sort_number",
+                        "goods_type_id",
+                        "goods_specification",
+                        "goods_place",
+                        "goods_supplier",
+                        "goods_unit",
+                        "goods_bazaar_money",
+                        "goods_bottom_money",
+                        "goods_keyword",
+                        "goods_abstract",
+                        "goods_detail",
+           ]);
+           $bool = db("goods")->insert($goods_data);
+           if($bool){
+               $goods_images = $request->only(["goods_images"]);
+               if(!empty($goods_images)){
+                   $dir_name = "/public/upload".date("Y-m-d");
+               }
+           }
+       }
     }
 }
