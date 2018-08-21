f6040a1b071efd624399d123a425d4c96819f743
Your Name <you@example.com>
2018-08-21 17:39:26 +0800

    zsw

--- application/route.php
+++ application/route.php
@@ -62,6 +62,7 @@ Route::group("",[
     'order_details'=>"index/Order/details",//订单详情
     'order_id'=>"index/Order/ajax_id",//订单详情
 
+    'check_logistic'=>"index/Order/logistic",//查看物流
     'order_myorder'=>"index/Order/myorder",//我的订单
     'order_wait_pay'=>"index/Order/wait_pay",//待支付
     'order_wait_deliver'=>"index/Order/wait_deliver",//待发货
