c1ab15c980c904339712254598afd71030876e68
lihuosheng <1061525487@qq.com>
2018-08-28 15:17:09 +0800

    l

---
+++ application/index/controller/Chat.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 李火生
+ * Date: 2018/8/28
+ * Time: 15:15
+ */
+namespace  app\index\controller;
+
+use think\Controller;
+
+class  Chat extends  Controller{
+
+
+
+    public function  Chat(){
+        return view('chat');
+    }
+
+}
