09220c875aa880253d89430fdda9dd45e260a801
lihuosheng <1061525487@qq.com>
2018-08-29 12:22:34 +0800

    聊天功能开始

---
+++ application/admin/controller/Chat.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 李火生
+ * Date: 2018/8/29
+ * Time: 11:19
+ */
+namespace  app\admin\controller;
+use think\Controller;
+
+class  Chat extends Controller{
+
+    public function index(){
+        return view('index');
+    }
+}
