cb5bb2e6cea592dc8b913876492f05ed309ed20f
chenxu11oopp <you@example.com>
2018-09-17 17:17:11 +0800

    chenxu

---
+++ application/admin/controller/Serve.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/9/17
+ * Time: 17:15
+ */
+
+namespace app\admin\controller;
+use think\Controller;
+
+class Serve extends Controller{
+
+    
+
+}
