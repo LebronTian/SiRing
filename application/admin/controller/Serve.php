ecfb6e70aa7d002e0ec5131eb1357bdb39140963
chenxu11oopp <you@example.com>
2018-09-18 11:45:12 +0800

    chenxu

--- application/admin/controller/Serve.php
+++ application/admin/controller/Serve.php
@@ -12,5 +12,23 @@ use think\Controller;
 class Serve extends Controller{
 
 
+    /**
+     * 售后维修处理
+     * 陈绪
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|\think\response\View
+     */
+    public function index(){
+        return view("serve_index");
+    }
+
+
+    /**
+     * 售后原因处理
+     * 陈绪
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|\think\response\View
+     */
+    public function listing(){
+        return view("serve_listing");
+    }
 
 }
