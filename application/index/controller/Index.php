830f2639835ea85a1836d9d6acab2e615f096bcd
chenxu11oopp <you@example.com>
2018-09-18 10:31:32 +0800

    chenxu

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -20,13 +20,14 @@ class Index extends Controller
             $seckill = db("seckill")->select();
             $goods = db("goods")->where("goods_status", 1)->select();
             return ajax_success("获取成功",array('goods_type'=>$goods_type,"seckill"=>$seckill,"goods"=>$goods));
-        }else{
+        }
+        if($request->isGet()){
             $goods_type = db("goods_type")->where("pid", "0")->order("sort_number")->select();
             $seckill = db("seckill")->select();
             $goods = db("goods")->where("goods_status", 1)->select();
-            return view("index",['goods_type'=>$goods_type,"seckill"=>$seckill,"goods"=>$goods]);
-
+            $this->assign(['goods_type'=>$goods_type,"seckill"=>$seckill,"goods"=>$goods]);
         }
+        return view("index");
     }
 
 }
