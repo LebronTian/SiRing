1ba4f2c8deb06c5aa122d05ad1a7db65b2a6d3cd
chenxu11oopp <you@example.com>
2018-09-28 10:00:06 +0800

    chenxu

--- application/index/controller/Discounts.php
+++ application/index/controller/Discounts.php
@@ -43,15 +43,12 @@ class Discounts extends Base{
                 if($bool == true){
                     db("discounts")->where("id",$discounts_id['id'])->update(["user_id"=>$user_id['user_id']]);
                 }
-                $discounts_data = db("discounts")->where("id",$discounts_id['id'])->where("user_id",$user_id["user_id"])->select();
-                $this->assign("discounts_data",$discounts_data);
             }
         }
         $time = time();
         db("discounts")->where("over_time","<",$time)->update(['status'=>3]);
-        $discounts_status = db("discounts")->where("status","3")->select();
-        $discounts = db("discounts")->where("user_id",$user_id["user_id"])->select();
-        return view("discounts_my",["discounts_status"=>$discounts_status,"discounts"=>$discounts]);
+        $discounts = db("discounts")->where("user_id",$user["id"])->select();
+        return view("discounts_my",["discounts"=>$discounts]);
     }
 
 }
