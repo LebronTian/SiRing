d1ffc6974ca177b29078444aeaad53cb8762a535
chenxu11oopp <you@example.com>
2018-10-10 17:13:12 +0800

    chenxu

--- application/index/controller/Seckill.php
+++ application/index/controller/Seckill.php
@@ -34,22 +34,21 @@ class Seckill extends Controller{
             $type_id = Session::get("type_id");
             $goods = db("goods")->where("goods_type_id", $type_id)->select();
             $images = db("seckill")->where("type_id", $type_id)->select();
-            $over_time = Session::get("over_time");
+            $over_time = $images[0]["over_time"];
             $time = time();
             if (!empty($over_time)) {
-                $over_time = Session::get("over_time");
+                $over_time = $images[0]["over_time"];
                 $str_time = $over_time;
                 if ($over_time - $time <= 0) {
                     $over_time = date("Y-m-d H:i:s", strtotime("+3 day"));
                     $str_time = strtotime($over_time);
-                    Session("over_time", $str_time);
                 }
 
             } else {
                 $over_time = date("Y-m-d H:i:s", strtotime("+3 day"));
                 $str_time = strtotime($over_time);
-                Session("over_time", $str_time);
             }
+            //获取time时间
             return ajax_success("获取成功", array("goods" => $goods, "images" => $images, "time" => $str_time));
         }
         return view("seckill_index");
