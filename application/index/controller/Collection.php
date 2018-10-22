f9ecdac897336b48caba8a4b6b98f3f222f3a5e5
lihuosheng <1061525487@qq.com>
2018-10-22 15:57:40 +0800

    la

--- application/index/controller/Collection.php
+++ application/index/controller/Collection.php
@@ -26,6 +26,7 @@ class  Collection extends Base{
                 ->field("tb_collection.*,tb_goods.goods_bottom_money goods_bottom_money,tb_goods.goods_name goods_name,tb_goods.goods_show_images goods_show_images")
                 ->join("tb_goods","tb_collection.goods_id=tb_goods.id and tb_collection.user_id=$member_id",'left')
                 ->order('id','desc')
+                ->where('user_id',$member_id)
                 ->select();
             if(!empty($data)){
 //                $this->assign('data',$data);
