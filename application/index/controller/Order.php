35dad43575b6579e8353f9e0f2f425c3ff701d94
lihuosheng-top <1061525487@qq.com>
2018-11-05 16:07:38 +0800

    购物车结算对接支付宝

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -265,9 +265,9 @@ class Order extends Controller {
                                         'order_information_number' => $create_time . $member['id'],//时间戳+用户id构成订单号
                                         'shopping_shop_id' => $v['id']
                                     ];
+                                    $res =Db::name('order')->insertGetId($datas);
                                     /*下单成功对购物车里面对应的商品进行删除*/
                                 }
-                            $res =Db::name('order')->insertGetId($datas);
                                 if($res){
                                     $res_one =Db::name('shopping')->where($where)->delete();
                                     if($res_one){
